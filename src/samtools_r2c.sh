44365b8fe36c76b250b21178387a0a497647119c
weidagong <wdgong@hotmail.com>
2018-11-21 09:59:12 -0500

    scripts

--- src/samtools_r2c.sh
+++ src/samtools_r2c.sh
@@ -14,4 +14,4 @@ sam=`ls $1*.sam`
 samtools view -Sb -f 0x2 $sam -o $1.bam && samtools sort -o $1.sort.bam $1.bam && samtools idxstats $1.sort.bam > $1_idxstats.tab
 touch $1.stats.txt
 samtools flagstat $1.sort.bam >> $1.stats.txt
-samtools view $1.sort.bam | cut -f 3 > $1.cts.tmp && sort $1.cts_f4.tmp | uniq -c > $1.counts.tab
+samtools view $1.sort.bam | cut -f 3 > $1.cts.tmp && sort $1.cts.tmp | uniq -c > $1.counts.tab
